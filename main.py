26af2e091092076ad2263343656ca7ca917b0058
roman <gastursolar@gmail.com>
2026-01-13 11:43:25 +0500

    delete debug lines

--- main.py
+++ main.py
@@ -31,44 +31,35 @@ from iceberg.connect import create_iceberg_table, get_iceberg_catalog
 from s3.connect import get_s3_connection
 from scripts.etl.parquet_load import write_parquet
 from scripts.etl.read_logs import read_logs
+from scripts.insert_logs import insert_logs
+from scripts.logs_generation import generate_logs
 
 load_dotenv()
 
 
 def main():
-    print(f"{get_database_url() =}")
     engine = create_engine(get_database_url())
-    # asyncio.run(insert_logs(engine, list(generate_logs(10000))))
+    asyncio.run(insert_logs(engine, list(generate_logs(10000))))
     df = asyncio.run(read_logs(engine))
     s3_connection = get_s3_connection(
         key=os.environ["MINIO_ROOT_USER"], secret=os.environ["MINIO_ROOT_PASSWORD"]
     )
+
     table = pa.Table.from_pandas(df)
-    print(s3_connection)
     write_parquet(
         table=table,
         where="logs-bucket/parquet/web_logs/web_logs.parquet",
         filesystem=s3_connection,
     )
-    print(
-        pq.read_table(
-            "logs-bucket/parquet/web_logs/web_logs.parquet",
-            filesystem=s3_connection,
-        )
-        .to_pandas()
-        .head()
-    )
     iceberg_catalog = get_iceberg_catalog(
         os.environ["MINIO_ROOT_USER"], os.environ["MINIO_ROOT_PASSWORD"]
     )
-    print(f"{iceberg_catalog =}")
 
-    iceberg_table = create_iceberg_table(iceberg_catalog, "logs_table")
-    print(f"{iceberg_table =}")
+    create_iceberg_table(iceberg_catalog, "web_logs")
 
-    table = iceberg_catalog.load_table(("default", "logs_table"))
-    print(f"{table=}")
+    table = iceberg_catalog.load_table(("default", "web_logs"))
     arrow_table = pa.Table.from_pandas(df)
+
     # downcast timestamp
     arrow_table = arrow_table.set_column(
         arrow_table.schema.get_field_index("timestamp"),
